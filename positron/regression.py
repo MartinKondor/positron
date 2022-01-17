5d09b611b8ff41bc5c04f04f9e480e4a9e08b326
Martin Kondor <martinkondor@gmail.com>
2022-01-17 17:14:20 +0100

    Adding linear regression

--- positron/regression.py
+++ positron/regression.py
@@ -8,11 +8,6 @@ import calculating as calc
 Linear regression
 """
 def linear(xtrain, ytrain, xtest, epochs=1000, learning_rate=0.000001):
-    
-    print(xtrain.shape, ytrain.shape)
-
-    # Least Squares
-    a = ytrain.mean()
-    b = xtrain.mean()
-
-    return a + xtest*b
+    _slope = xtrain - xtrain.mean()
+    slope = (_slope * (ytrain - ytrain.mean())).sum() / (_slope**2).sum()
+    return (ytrain.mean() - slope*xtrain.mean()) + xtest * slope
